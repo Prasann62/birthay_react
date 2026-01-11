12c00338fd552b261f9de2007aaef2b9aecaf49a
Prasann62 <prasannakumar95657@gmail.com>
2026-01-11 19:55:59 +0530

    Implement synced blinking banner text

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,11 +4,37 @@ const Banner = ({ visible }) => {
     return (
         <div className="row">
             <div className="col-12 text-center">
-                <img
-                    src="/banner.png"
-                    className={`bannar ${visible ? 'bannar-come' : ''}`}
-                    alt="Happy Birthday Banner"
-                />
+                <h1
+                    className={`bannar banner-text ${visible ? 'bannar-come' : ''}`}
+                >
+                    {/* H (Yellow: 1s) */}
+                    <span className="blink-yellow">H</span>
+                    {/* A (Red: 1.2s) */}
+                    <span className="blink-red">A</span>
+                    {/* P (Blue: 1.4s) */}
+                    <span className="blink-blue">P</span>
+                    {/* P (Green: 1.8s) */}
+                    <span className="blink-green">P</span>
+                    {/* Y (Pink: 2s) */}
+                    <span className="blink-pink">Y</span>
+                    &nbsp;
+                    {/* B (Orange: 2.2s) */}
+                    <span className="blink-orange">B</span>
+                    {/* I (Yellow) */}
+                    <span className="blink-yellow">I</span>
+                    {/* R (Red) */}
+                    <span className="blink-red">R</span>
+                    {/* T (Blue) */}
+                    <span className="blink-blue">T</span>
+                    {/* H (Green) */}
+                    <span className="blink-green">H</span>
+                    {/* D (Pink) */}
+                    <span className="blink-pink">D</span>
+                    {/* A (Orange) */}
+                    <span className="blink-orange">A</span>
+                    {/* Y (Yellow) */}
+                    <span className="blink-yellow">Y</span>
+                </h1>
             </div>
         </div>
     );
